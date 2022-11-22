e5997309cd40a32c63d5fa461746d9dabc1dfea2
Alex Nichol <unixpickle@gmail.com>
2022-11-22 10:24:26 -0500

    less loss memory usage

--- glove/loss.py
+++ glove/loss.py
@@ -12,15 +12,28 @@ class SquaredError(torch.autograd.Function):
     def forward(ctx, x, y, w):
         ctx.save_for_backward(x, y, w)
         with torch.no_grad():
-            diff = x - y
-            diff.pow_(2.0)
-            diff.mul_(w)
-            return diff.sum()
+            x = x.reshape(-1)
+            y = y.reshape(-1)
+            w = w.reshape(-1)
+
+            # Do the large accumulation in the highest possible precision.
+            total_loss = torch.zeros((), dtype=torch.float64, device=x.device)
+
+            # Chunk intermediate values to avoid extra allocation of size x.
+            chunk_size = 2**20
+            for i in range(0, len(x), chunk_size):
+                diff = (
+                    w[i : i + chunk_size].to(x)
+                    * (x[i : i + chunk_size] - y[i : i + chunk_size].to(x)) ** 2
+                )
+                total_loss += diff.to(total_loss).sum()
+            return total_loss
 
     @staticmethod
     def backward(ctx, grad_output):
         x, y, w = ctx.saved_tensors
         res = x - y
         res.mul_(w)
-        res.mul_(2.0 * grad_output)
+        res.mul_(grad_output)
+        res.mul_(2.0)
         return res, None, None
