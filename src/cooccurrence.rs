e62a5aa73a186d4b4ca97793621f714b4918f283
Alex Nichol <unixpickle@gmail.com>
2023-01-30 11:54:06 -0500

    fix query

--- src/cooccurrence.rs
+++ src/cooccurrence.rs
@@ -242,7 +242,7 @@ async fn read_all_store_locations_discover_sqlite3(
         let mut res = HashMap::<String, Vec<GeoCoord>>::new();
         let mut query = db.prepare(
             "
-                SELECT (name, lat, lon) FROM poi ORDER BY name WHERE name IN (
+                SELECT name, lat, lon FROM poi WHERE name IN (
                     SELECT name FROM poi GROUP BY name HAVING COUNT(*) >= ?1
                 )
             ",
