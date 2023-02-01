f3f7d5e28e59b2883ee05a726c2aafab55224d98
Alex Nichol <unixpickle@gmail.com>
2023-01-31 21:50:04 -0800

    fix categories script

--- src/categories.rs
+++ src/categories.rs
@@ -1,8 +1,6 @@
 use crate::bing_maps::Client;
 use crate::bing_maps::PointOfInterest;
-use crate::cooccurrence::read_all_store_locations;
-use crate::geo_coord::GeoCoord;
-use crate::geo_coord::GlobeBounds;
+use crate::discover::read_all_store_locations_discover_json;
 use crate::task_queue::TaskQueue;
 use clap::Parser;
 use rand::seq::IteratorRandom;
@@ -25,9 +23,6 @@ pub struct CategoriesArgs {
     #[clap(short, long, value_parser, default_value_t = 0)]
     min_count: usize,
 
-    #[clap(short, long, value_parser, default_value_t = GlobeBounds::Globe)]
-    bounds: GlobeBounds,
-
     #[clap(value_parser)]
     discover_out: String,
 
@@ -37,9 +32,10 @@ pub struct CategoriesArgs {
 
 pub async fn categories(cli: CategoriesArgs) -> anyhow::Result<()> {
     println!("loading locations...");
-    let locations = read_all_store_locations(&cli.discover_out, cli.bounds, cli.min_count).await?;
+    let locations =
+        read_all_store_locations_discover_json(&cli.discover_out, cli.min_count).await?;
     let total_tasks = locations.len();
-    let task_queue: TaskQueue<(String, Vec<GeoCoord>)> = locations.into();
+    let task_queue: TaskQueue<(String, Vec<PointOfInterest>)> = locations.into();
     println!("total locations: {}", total_tasks);
 
     println!("creating workers...");
@@ -53,15 +49,7 @@ pub async fn categories(cli: CategoriesArgs) -> anyhow::Result<()> {
         let mut rng = rand::rngs::StdRng::seed_from_u64(i as u64);
         spawn(async move {
             let mut client = Client::new();
-            while let Some((name, coords)) = task_queue_clone.pop().await {
-                let pois = coords
-                    .into_iter()
-                    .map(|coord| PointOfInterest {
-                        id: "".to_owned(),
-                        name: name.clone(),
-                        location: coord,
-                    })
-                    .collect();
+            while let Some((name, pois)) = task_queue_clone.pop().await {
                 let obj =
                     get_categories(&mut client, &mut rng, pois, sample_locations, max_retries)
                         .await;
